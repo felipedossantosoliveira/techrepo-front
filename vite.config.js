1f846e63f91f09d46ae6348f1ace4a7444c52ed5
felipe <felipedossantosads@gmail.com>
2023-05-04 12:49:30 -0300

    chore: define vite port to dev

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+    port: 5173, // This is the port which we will use in docker
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
