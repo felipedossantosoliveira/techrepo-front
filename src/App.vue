7ab3578b14a5215236130ca7a08b2024d4165f5f
felipe <felipedossantosads@gmail.com>
2023-05-16 23:48:43 -0300

    feat: nova identidade visual

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { reactive } from 'vue'
+import LogoOnion from '@/components/icons/LogoOnion.vue'
 
+const show = reactive({
+  value: false,
+})
 
 </script>
 
 <template>
-  <header>
-    <div class="flex mx-10 my-4 rounded-2xl px-10 py-4 items-center bg-zinc-800 justify-between">
+  <!-- <header> -->
+    <!-- <div class="flex mx-10 my-4 rounded-2xl px-10 py-4 items-center bg-zinc-800 justify-between">
         <p class="bg-gradient-to-r from-lime-700 to-green-500 bg-clip-text text-4xl font-extrabold text-transparent">
         techrepo
         </p>
@@ -15,10 +20,64 @@ import { RouterView } from 'vue-router'
             <path fill-rule="evenodd" clip-rule="evenodd" d="M11.9642 0C5.34833 0 0 5.5 0 12.3042C0 17.7432 3.42686 22.3472 8.18082 23.9767C8.77518 24.0992 8.9929 23.712 8.9929 23.3862C8.9929 23.101 8.97331 22.1232 8.97331 21.1045C5.64514 21.838 4.95208 19.6378 4.95208 19.6378C4.41722 18.2118 3.62473 17.8452 3.62473 17.8452C2.53543 17.0915 3.70408 17.0915 3.70408 17.0915C4.91241 17.173 5.54645 18.3545 5.54645 18.3545C6.61592 20.2285 8.33927 19.699 9.03257 19.373C9.13151 18.5785 9.44865 18.0285 9.78539 17.723C7.13094 17.4377 4.33812 16.3785 4.33812 11.6523C4.33812 10.3078 4.81322 9.20775 5.56604 8.35225C5.44727 8.04675 5.03118 6.7835 5.68506 5.09275C5.68506 5.09275 6.69527 4.76675 8.97306 6.35575C9.94827 6.08642 10.954 5.9494 11.9642 5.94825C12.9744 5.94825 14.0042 6.091 14.9552 6.35575C17.2332 4.76675 18.2434 5.09275 18.2434 5.09275C18.8973 6.7835 18.481 8.04675 18.3622 8.35225C19.1349 9.20775 19.5904 10.3078 19.5904 11.6523C19.5904 16.3785 16.7976 17.4172 14.1233 17.723C14.5592 18.11 14.9353 18.8432 14.9353 20.0045C14.9353 21.6545 14.9158 22.9787 14.9158 23.386C14.9158 23.712 15.1337 24.0992 15.7278 23.977C20.4818 22.347 23.9087 17.7432 23.9087 12.3042C23.9282 5.5 18.5603 0 11.9642 0Z" fill="currentColor"/>
         </svg>
         </a>
+    </div> -->
+  <!-- </header> -->
+  <!-- <div class="bg-zinc-800 h-screen flex w-screen pr-4 py-4">
+    <div class="h-full h
+    w-16">
+      <div>
+        <svg class="w-10" viewBox="0 0 700 621" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M575 452.5V236L700 320.5V537L575 452.5Z" fill="url(#paint0_linear_8_2)"/>
+<path d="M600 320.5H700L575 236H475L537.5 278.25L600 320.5Z" fill="#D9D9D9"/>
+<path d="M475 236V168L600 252.5V320.5L475 236Z" fill="#BEBEBE"/>
+<path d="M637.5 210.25L575 168H475L600 252.5H700L637.5 210.25Z" fill="#979797"/>
+<path d="M637.5 42.25L575 0V168L700 252.5V84.5L637.5 42.25Z" fill="url(#paint1_linear_8_2)"/>
+<path d="M225 0H400H575L700 84.5H350L287.5 42.25L225 0Z" fill="#D9D9D9"/>
+<path d="M350 536.5V84L475 168.5V621L350 536.5Z" fill="#3E8581"/>
+<path d="M0 84H350L475 168.5H125L0 84Z" fill="#6AE3DD"/>
+<defs>
+<linearGradient id="paint0_linear_8_2" x1="700" y1="198" x2="575" y2="365" gradientUnits="userSpaceOnUse">
+<stop stop-color="#4F4F4F"/>
+<stop offset="1" stop-color="#A2A2A2"/>
+</linearGradient>
+<linearGradient id="paint1_linear_8_2" x1="637.5" y1="2.49336e-06" x2="468.5" y2="230.5" gradientUnits="userSpaceOnUse">
+<stop stop-color="#606060"/>
+<stop offset="0.625" stop-color="#A2A2A2"/>
+</linearGradient>
+</defs>
+</svg>
+      </div>
+    </div>
+    <div class="bg-zinc-900 h-full w-full rounded-xl">
+    </div>
+  </div> -->
+  <header>
+    <div class="fixed w-full h-full opacity-5 flex items-center justify-center -z-10">
+      <LogoOnion class="h-full p-4"/>
+    </div>
+    <div>
+      <div class="flex my-4 rounded-2xl px-10 py-4 items-center justify-between">
+        <div class="text-2xl font-bold px-6 h-10 flex items-center space-x-4 bg-zinc-900 text-neutral-200 rounded-full">
+          <LogoOnion class="w-10" />
+          <p>
+            techrepo
+          </p>
+        </div>
+        <div class="flex justify-between px-10 bg-zinc-900 h-10 rounded-full space-x-6">
+          <router-link to="/" class="flex items-center justify-center font-semibold text-neutral-200">
+            Home
+          </router-link>
+          <router-link to="/brands" class="flex items-center justify-center font-semibold text-neutral-200">
+            Brands
+          </router-link>
+          <router-link to="/devices" class="flex items-center justify-center font-semibold text-neutral-200">
+            Devices
+          </router-link>
+        </div>
+      </div>
     </div>
   </header>
   <div class="px-10">
     <router-view />
   </div>
-    
 </template>
---
+++ src/components/icons/LogoOnion.vue
@@ -0,0 +1,58 @@
+<template>
+	<svg viewBox="0 0 860 448" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M550.429 190.451C666.364 154.012 741.007 34 756.888 34L818.429 156.784C810.488 174.608 674.305 185.302 590.133 236L550.429 190.451Z" fill="url(#paint0_linear_10_2)" stroke="url(#paint1_linear_10_2)" stroke-width="8"/>
+<path d="M825.917 80.2659C834.009 98.0628 836.38 116.231 833.861 131.128C831.337 146.053 824.031 157.246 813.155 162.191C801.358 167.555 783.884 170.841 767.469 168.37C751.167 165.916 736.352 157.894 728.687 141.034C720.858 123.814 721.7 100.145 727.813 79.0775C730.85 68.613 735.13 59.0057 740.084 51.3949C745.085 43.7133 750.547 38.4103 755.794 36.0248C766.67 31.08 779.907 32.9331 792.813 40.8431C805.694 48.7385 817.826 62.469 825.917 80.2659Z" fill="url(#paint2_linear_10_2)" stroke="url(#paint3_linear_10_2)" stroke-width="8"/>
+<path d="M314.429 180.451C195.899 144.012 119.585 24 103.347 24L40.429 146.784C48.5475 164.608 187.78 175.302 273.836 226L314.429 180.451Z" fill="url(#paint4_linear_10_2)" stroke="url(#paint5_linear_10_2)" stroke-width="8"/>
+<path d="M34.4462 69.4104C26.2254 87.4918 23.8153 105.953 26.3754 121.094C28.9401 136.263 36.3675 147.65 47.4359 152.682C59.4257 158.134 77.1811 161.473 93.8624 158.962C110.431 156.468 125.496 148.313 133.291 131.168C141.249 113.664 140.391 89.6126 134.181 68.212C131.096 57.581 126.748 47.8196 121.713 40.0854C116.632 32.2803 111.077 26.8846 105.734 24.4554C94.666 19.4231 81.2024 21.3124 68.0862 29.3516C54.9939 37.3762 42.6669 51.3289 34.4462 69.4104Z" fill="url(#paint6_linear_10_2)" stroke="url(#paint7_linear_10_2)" stroke-width="8"/>
+<path d="M273.629 217.859C255.769 240.228 245.429 266.748 245.429 295.183C245.429 365.998 340.398 403.15 404.825 430.005C401.091 434.632 395.864 443.884 404.825 443.884H451.733C455.063 444.545 460.922 442.695 457.72 430.005C525.553 405.963 610.429 364.439 610.429 295.183C610.429 266.748 600.089 240.228 582.229 217.859C547.403 174.24 456.494 129.828 443.799 87.0022C435.4 58.6716 412.339 73.9954 410.076 87.0022C402.141 132.604 302.956 181.127 273.629 217.859Z" fill="white" stroke="#B37600" stroke-width="8" stroke-linejoin="round"/>
+<g filter="url(#filter0_d_10_2)">
+<path d="M457.555 347.392C474.338 315.449 429.149 289.507 422.435 246.29C412.366 283.87 378.341 313.194 397.138 349.271M453.517 374.091C541.174 298.832 453.517 275.106 425.791 184.282C385.512 274.475 311.445 306.741 397.138 376.068M366.378 376.068C246.091 277.083 388.681 225.676 425.791 144.612C457.555 215.791 610.489 296.855 475.302 376.068M344.593 376.068C172.815 273.129 397.138 174.27 425.791 111C491.145 199.973 685.824 289.928 491.145 382" stroke="#B27600" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<filter id="filter0_d_10_2" x="256.428" y="87.9999" width="343.006" height="319.001" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="1"/>
+<feGaussianBlur stdDeviation="10"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_10_2"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_10_2" result="shape"/>
+</filter>
+<linearGradient id="paint0_linear_10_2" x1="752.918" y1="109.255" x2="572.5" y2="218.563" gradientUnits="userSpaceOnUse">
+<stop stop-color="#025000"/>
+<stop offset="0.317612" stop-color="#025000"/>
+<stop offset="1" stop-color="#038800"/>
+</linearGradient>
+<linearGradient id="paint1_linear_10_2" x1="741.007" y1="59.7451" x2="570.743" y2="252.252" gradientUnits="userSpaceOnUse">
+<stop stop-color="#025000"/>
+<stop offset="1" stop-color="#038800"/>
+</linearGradient>
+<linearGradient id="paint2_linear_10_2" x1="824.094" y1="73.3549" x2="725.044" y2="146.652" gradientUnits="userSpaceOnUse">
+<stop stop-color="#011D00"/>
+<stop offset="1" stop-color="#025000"/>
+</linearGradient>
+<linearGradient id="paint3_linear_10_2" x1="820.132" y1="61.469" x2="717.12" y2="150.614" gradientUnits="userSpaceOnUse">
+<stop stop-color="#038800"/>
+<stop offset="1" stop-color="#012E00"/>
+</linearGradient>
+<linearGradient id="paint4_linear_10_2" x1="107.407" y1="99.2549" x2="289.648" y2="212.139" gradientUnits="userSpaceOnUse">
+<stop stop-color="#025000"/>
+<stop offset="0.317612" stop-color="#025000"/>
+<stop offset="1" stop-color="#038800"/>
+</linearGradient>
+<linearGradient id="paint5_linear_10_2" x1="119.585" y1="49.7451" x2="289.348" y2="245.983" gradientUnits="userSpaceOnUse">
+<stop stop-color="#025000"/>
+<stop offset="1" stop-color="#038800"/>
+</linearGradient>
+<linearGradient id="paint6_linear_10_2" x1="36.3542" y1="62.4182" x2="136.934" y2="136.847" gradientUnits="userSpaceOnUse">
+<stop stop-color="#011D00"/>
+<stop offset="1" stop-color="#025000"/>
+</linearGradient>
+<linearGradient id="paint7_linear_10_2" x1="40.3774" y1="50.3487" x2="144.98" y2="140.87" gradientUnits="userSpaceOnUse">
+<stop stop-color="#038800"/>
+<stop offset="1" stop-color="#012E00"/>
+</linearGradient>
+</defs>
+</svg>
+</template>
